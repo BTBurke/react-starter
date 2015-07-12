ed563c56350e7f70c038ad8ec29cdf859a49df9b
Bryan Burke <bryan.t.burke@gmail.com>
2015-07-12 09:57:40 +0000

    add devserver

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 module.exports = {
     entry: {
-        app: './src/js/app'
+        app: ['webpack-dev-server/client?http://localhost:8080', './src/js/app']
     },
     output: {
         path: './dist',
         filename: 'assets/scripts/app.js'
+    },
+    devServer: {
+        contentBase: "./dist",
+        progress: true,
+        noInfo: true,
+        hot: true,
+        colors: true,
+        inline: true,
+        port: 8080
     },
     module: {
         loaders: [
